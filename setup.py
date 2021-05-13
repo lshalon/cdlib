dffd97bf2f2c905ebf71803883bca8b63f6a24da
lshalon <shalon.liam@gmail.com>
2021-05-13 14:54:08 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -39,7 +39,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
 
-setup(name='personal-cdlib',
+setup(name='personalcdlib',
       version='0.1.10',
       license='BSD-Clause-2',
       description='Community Discovery Library',
